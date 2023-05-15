febdc88810eaf061254acc68492210734972a5cf
DESKTOP-VL8BMAO\mash9 <hideyo.arukustam58@gmail.cim>
2023-05-15 23:51:14 +0900

    デバッカーの導入 シードの実行

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,5 +13,22 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
 
+        \DB::table('users')->insert([
+        [
+            'id' => '1',
+            'over_name' => '山田',
+            'under_name' => '太郎',
+            'over_name_kana' => 'ヤマダ',
+            'under_name_kana' => 'タロウ',
+            'mail_address' => 'y_tarou0401@example.com',
+            'sex' => '1',
+            'birth_day' => '2000-04-01',
+            'role' => '4',
+            'remember_token' => '',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+            'deleted_at' => date('Y-m-d H:i:s'),
+        ]
+        ]);
     }
 }
